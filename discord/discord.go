a93e867aec87d314d493d78bb06b4117e6b19773
eraiza0816 <programeraiza@hotmail.co.jp>
2025-03-18 15:58:12 +0900

    Chore: 開発環境

--- discord/discord.go
+++ discord/discord.go
@@ -42,7 +42,7 @@ func StartBot(cfg *config.Config) error {
 	commands := []*discordgo.ApplicationCommand{
 		{
 			Name:        "chat",
-			Description: "おしゃべりしようよ",
+			Description: "おしゃべりしようよ dev",
 			Options: []*discordgo.ApplicationCommandOption{
 				{
 					Type:        discordgo.ApplicationCommandOptionString,
@@ -54,7 +54,7 @@ func StartBot(cfg *config.Config) error {
 		},
 		{
 			Name:        "reset",
-			Description: "あなたとのチャット履歴をリセット",
+			Description: "あなたとのチャット履歴をリセット dev",
 		},
 	}
 
